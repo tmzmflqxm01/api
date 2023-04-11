e800a0e85ee4aa3f32afd1e7a63d46d54c928879
tmzmflqxm01 <twotree1234@naver.com>
2023-04-11 15:33:08 +0900

    first commit

---
+++ api/todo.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const todoController = require('./_controller/todoController');
+router.get('/', async (req,res)=>{
+    //const result = await todoController.getTest();
+    const result = await todoController.list(req);
+    res.json(result);
+})
+
+// create
+router.post("/", async (req, res) => {
+    const result = await todoController.create(req);
+    res.json(result);
+})
+
+// update
+router.put('/:id', async (req,res)=>{
+    const result = await todoController.update(req);
+    res.json(result);
+})
+
+// delete
+router.delete('/:id', async (req,res)=>{
+    const result = await todoController.delete(req);
+    res.json(result);
+})
+
+// truncate, dummy insert
+router.post('/reset', async (req,res)=>{
+    const result = await todoController.reset(req);
+    res.json(result);
+})
+
+
+
+module.exports = router;
